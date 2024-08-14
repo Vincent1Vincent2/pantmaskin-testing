23f2d39ade63fc25edde24d9e757c1a7cd1c38b1
Vincent <Vincent.ahlin99@gmail.com>
2024-08-14 15:43:59 +0200

    Create a better function to register clicked items

--- src/components/RecyclingMachine/RecyclingMachine.test.tsx
+++ src/components/RecyclingMachine/RecyclingMachine.test.tsx
@@ -3,8 +3,9 @@ import { describe, expect, it } from "vitest";
 import RecyclingMachine from "./RecyclingMachine";
 
 const clickItems = (itemType: "can" | "bottle", count: number) => {
-  for (let i = 1; i <= count; i++) {
-    fireEvent.click(screen.getByTestId(`${itemType}-${i}`));
+  const elements = screen.getAllByTestId(new RegExp(`^${itemType}-`));
+  for (let i = 0; i < count && i < elements.length; i++) {
+    fireEvent.click(elements[i]);
   }
 };
 
@@ -45,20 +46,24 @@ describe("RecyclingMachine", () => {
     expect(screen.queryByTestId("counted-cans")).not.toBeInTheDocument();
     expect(screen.queryByTestId("counted-bottles")).not.toBeInTheDocument();
   });
-
   it("should increment the corresponding type, either can or bottle", () => {
     render(<RecyclingMachine />);
     fireEvent.click(screen.getByTestId("screen"));
+
     expect(screen.queryByTestId("counted-cans")).toHaveTextContent("0");
     expect(screen.queryByTestId("counted-bottles")).toHaveTextContent("0");
+
     clickItems("can", 1);
+
     expect(screen.queryByTestId("counted-cans")).toHaveTextContent("1");
     expect(screen.queryByTestId("counted-bottles")).toHaveTextContent("0");
     clickItems("bottle", 1);
+
     expect(screen.queryByTestId("counted-cans")).toHaveTextContent("1");
     expect(screen.queryByTestId("counted-bottles")).toHaveTextContent("1");
     clickItems("can", 2);
     clickItems("bottle", 2);
+
     expect(screen.queryByTestId("counted-cans")).toHaveTextContent("3");
     expect(screen.queryByTestId("counted-bottles")).toHaveTextContent("3");
   });
