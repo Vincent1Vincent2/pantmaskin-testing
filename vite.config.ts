451713a850fb846925b459dbb061eb7525b3313c
Vincent <Vincent.ahlin99@gmail.com>
2024-08-14 15:41:29 +0200

    id being same removed both instead of the one intended, some styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +7,9 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: ["src/test-setup.ts"],
-    coverage: { provider: "istanbul" },
+    coverage: {
+      provider: "istanbul",
+      exclude: [...coverageConfigDefaults.exclude, "src/main.tsx"],
+    },
   },
 });
